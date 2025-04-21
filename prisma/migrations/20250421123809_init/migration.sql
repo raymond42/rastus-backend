88058d47e28f2148e67a26994610bd2390d52435
Raymond <raymond42.gr@gmail.com>
2025-04-21 18:45:52 +0200

    add login for auth

--- prisma/migrations/20250421123809_init/migration.sql
+++ prisma/migrations/20250421123809_init/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "phone_number" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
