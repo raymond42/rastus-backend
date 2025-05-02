c7af4e7789181822322d45feac8b5783b7f6c312
Raymond <raymond42.gr@gmail.com>
2025-05-02 23:26:48 +0200

    Remove dist folder from repo

--- dist/src/auth/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AuthService } from './auth.service';
-import { LoginDto, LoginResponseDto } from './dto/login.dto';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    login(loginDto: LoginDto): Promise<LoginResponseDto>;
-}
