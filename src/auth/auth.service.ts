e348ca8ee24f43e61363b768f2aa5f14b9c70aae
Raymond <raymond42.gr@gmail.com>
2025-05-03 00:12:33 +0200

    fix bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
-import { genSaltSync, hashSync, compare } from 'bcrypt-ts';
+import { genSaltSync, hashSync, compare } from 'bcryptjs';
 import { LoginDto, LoginResponseDto } from './dto/login.dto';
 
 @Injectable()
